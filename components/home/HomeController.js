50231b513e8ad8d30159b46600387231f3eb589a
Alex Klibisz <aklibisz@utk.edu>
2015-03-29 12:41:39 -0400

    cleaned up FSAModel and commented

--- components/home/HomeController.js
+++ components/home/HomeController.js
@@ -3,7 +3,7 @@ app.controller('HomeController',
 
         //Initialize NFA and DFA -- called via ng-init
         $scope.initializeNFA = function() {
-            $scope.NFA = new FSAModel("#NFA", 310, 610, [{id: "A"}, {id: "B"}, {id: "C"}, {id: "D"}, {id: "E"}], []);
+            $scope.NFA = new FSAModel("#NFA", 310, 610, ["A", "B", "C", "D", "E"], []);
         }
 
         $scope.initializeDFA = function() {}
--- components/home/models/FSAModel.js
+++ components/home/models/FSAModel.js
@@ -9,32 +9,26 @@ app.service('FSAModel', function(Map) {
         this.links = links;
 
         this.addInitialNodes(nodes);
-
-        this.initialize = function() {}
-
-
-
-        this.setStartNode = function(element) {}
-
-        this.dragStart = function(d) {}
-
-
-
     }
 
     FSAModel.prototype = {
+        /**
+         * Validate the passed label;
+         * Create a node object with the label and coordinates;
+         * Add the object to the nodes map;
+         * Create, append svgNode as a container for the circle and the label;
+         * Create, append the circle;
+         * Create, append the label
+         */
         addNode: function(label, x, y) {
             //validate based on the label
             if (label.length === 0 || label.length > 3 || this.nodes.find(label) !== false) return false;
-
             var node = {
                 id: label,
                 x: x,
                 y: y
             };
-
             this.nodes.put(node.id, node);
-
             //create the container element and define drag behavior
             var svgNode = this.container.append("g")
                 .attr("transform", "translate(" + node.x + "," + node.y + ")")
@@ -53,16 +47,42 @@ app.service('FSAModel', function(Map) {
                 .style("cursor", "pointer");
         },
         /**
-         * Add a list of node objects passed from the controller
+         * nodes: list of character labels;
+         * iterate over the list;
+         * calculate x and y coordinates for a 4 * n/4 grid;
+         * call addNode() to add the nodes
          */
         addInitialNodes: function(nodes) {
             var i, x, y;
             for (i = 0; i < nodes.length; i++) {
-                x = (this.width / 4) * (i*1.0%4) + 50;
+                x = (this.width / 4) * (i * 1.0 % 4) + 50;
                 y = (this.height / 4) * Math.floor(i / 4) + 50;
-                this.addNode(nodes[i].id, x, y);
+                this.addNode(nodes[i], x, y);
             }
         },
+        selectNode: function(node) {
+            this.toggleNodeProperty(node, "circle", "selected");
+        },
+        deleteSelected: function() {
+            var nodes = this.nodes.contents;
+            for (var n in nodes) {
+                if (nodes[n].selected) {
+                    d3.select("#" + nodes[n].id).remove();
+                    this.nodes.remove(nodes[n].id);
+                }
+            }
+        },
+        setAcceptNodes: function() {
+            var nodes = this.nodes.contents,
+                node;
+            for (var n in nodes) {
+                if (nodes[n].selected) {
+                    node = d3.select("#" + nodes[n].id);
+                    this.toggleNodeProperty(node[0][0], "circle", "accept");
+                }
+            }
+        },
+        setStartNode: function() {},
         /**
          * Generic function for toggling a true/false property
          * for a node, its css class, and in its object.
@@ -81,31 +101,13 @@ app.service('FSAModel', function(Map) {
             }
             this.nodes.put(nodeObj.id, nodeObj);
         },
-        selectNode: function(node) {
-            this.toggleNodeProperty(node, "circle", "selected");
-        },
-        deleteSelected: function() {
-            var nodes = this.nodes.contents;
-            for (var n in nodes) {
-                if (nodes[n].selected) {
-                    d3.select("#" + nodes[n].id).remove();
-                    this.nodes.remove(nodes[n].id);
-                }
-            }
-        },
-        acceptSelected: function() {
-            var nodes = this.nodes.contents,
-                node;
-            for (var n in nodes) {
-                if (nodes[n].selected) {
-                    node = d3.select("#" + nodes[n].id);
-                    this.toggleNodeProperty(node[0][0], "circle", "accept");
-                }
-            }
-        }
     }
 
-    function dragMove(d) {
+    /**
+     * Node dragging behavior.
+     * Node is not dragged if the shift key is pressed.
+     */
+    function dragMove() {
         // Prevent drag if shift key pressed
         if (!d3.event.sourceEvent.shiftKey) {
             var x = d3.event.x;

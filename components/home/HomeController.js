66bef53d22bd34feaad18ccc008b45ed016b3b29
Alex Klibisz <aklibisz@vols.utk.edu>
2015-04-04 11:45:35 -0400

    copy/pasted force layout working

--- components/home/HomeController.js
+++ components/home/HomeController.js
@@ -5,14 +5,14 @@ app.controller('HomeController',
         $scope.initializeNFA = function() {
             $scope.NFA = new ForceFSM("#NFA", 310, 630, ['a', 'b'], [], []);
 
-            $scope.NFA.addState(new FSMState("1"));
-            $scope.NFA.addState(new FSMState("2"));
-            $scope.NFA.addState(new FSMState("3"));
-            $scope.NFA.addTransition(new FSMTransition("E", "1", "3"));
-            $scope.NFA.addTransition(new FSMTransition("a", "3", "1"));
-            $scope.NFA.addTransition(new FSMTransition("a,b", "2", "3"));
-            $scope.NFA.addTransition(new FSMTransition("a", "2", "2"));
-            $scope.NFA.addTransition(new FSMTransition("b", "1", "2"));
+            // $scope.NFA.addState(new FSMState("1"));
+            // $scope.NFA.addState(new FSMState("2"));
+            // $scope.NFA.addState(new FSMState("3"));
+            // $scope.NFA.addTransition(new FSMTransition("E", "1", "3"));
+            // $scope.NFA.addTransition(new FSMTransition("a", "3", "1"));
+            // $scope.NFA.addTransition(new FSMTransition("a,b", "2", "3"));
+            // $scope.NFA.addTransition(new FSMTransition("a", "2", "2"));
+            // $scope.NFA.addTransition(new FSMTransition("b", "1", "2"));
 
             $scope.NFA.start();
         }
